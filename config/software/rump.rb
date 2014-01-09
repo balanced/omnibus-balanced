a1d8e1645eb9aa1e2115d75b8b3afcec41d0b14b
Noah Kantrowitz <noah@coderanger.net>
2014-01-08 17:07:55 -0800

    Fix link on rebuild and install extras.

--- config/software/rump.rb
+++ config/software/rump.rb
@@ -43,8 +43,7 @@ build do
     end
   end
 
-  #command "#{install_dir}/embedded/bin/pip install --install-option=--prefix=#{install_dir}/embedded .[kazoo,raven,newrelic]", cwd: "#{project_dir}/src"
-  command "#{install_dir}/embedded/bin/pip install --install-option=--prefix=#{install_dir}/embedded .", cwd: "#{project_dir}/src"
-  command "ln -s #{install_dir}/embedded/bin/rump #{install_dir}/bin/rump"
-  command "ln -s #{install_dir}/embedded/bin/rumpd #{install_dir}/bin/rumpd"
+  command "#{install_dir}/embedded/bin/pip install --install-option=--prefix=#{install_dir}/embedded . kazoo>=1.3.1 newrelic>=1.13.1.31 raven==3.5.2", cwd: "#{project_dir}/src"
+  command "ln -fs #{install_dir}/embedded/bin/rump #{install_dir}/bin/rump"
+  command "ln -fs #{install_dir}/embedded/bin/rumpd #{install_dir}/bin/rumpd"
 end
