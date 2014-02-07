ca2e71c05d4bf6ba75324ea5f6a68ac875b47330
Marshall Jones <marshall.jones.1980@gmail.com>
2014-02-07 10:11:02 -0800

    Update brache.rb

--- config/software/brache.rb
+++ config/software/brache.rb
@@ -53,5 +53,5 @@ build do
 	  "PATH" => "/opt/brache/embedded/bin:#{ENV['PATH']}",
   }
 
-  command "#{install_dir}/embedded/bin/pip install --upgrade --install-option=--prefix=#{install_dir}/embedded file://#{project_dir}#egg=brache[user]", env: env
+  command "rm /tmp/brache-build && #{install_dir}/embedded/bin/pip install --upgrade --install-option=--prefix=#{install_dir}/embedded file://#{project_dir}#egg=brache[user] -b /tmp/brache-build", env: env
 end
