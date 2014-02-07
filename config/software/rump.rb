f9144c4e245c28bc4d487681749f496e4d298f16
Marshall Jones <marshall.jones.1980@gmail.com>
2014-02-06 18:27:10 -0800

    Update rump.rb

--- config/software/rump.rb
+++ config/software/rump.rb
@@ -45,8 +45,8 @@ build do
     end
   end
 
-  command "#{install_dir}/embedded/bin/pip install --install-option=--prefix=#{install_dir}/embedded file://#{project_dir}/src#egg=rump[kazoo,newrelic,raven]", cwd: "#{project_dir}/src"
-  command "#{install_dir}/embedded/bin/pip install --install-option=--prefix=#{install_dir}/embedded brache[router]"
+  command "#{install_dir}/embedded/bin/pip install --upgrade --install-option=--prefix=#{install_dir}/embedded file://#{project_dir}/src#egg=rump[kazoo,newrelic,raven]", cwd: "#{project_dir}/src"
+  command "#{install_dir}/embedded/bin/pip install --upgrade --install-option=--prefix=#{install_dir}/embedded brache[router]"
   command "ln -fs #{install_dir}/embedded/bin/rump #{install_dir}/bin/rump"
   command "ln -fs #{install_dir}/embedded/bin/rumpd #{install_dir}/bin/rumpd"
 end
