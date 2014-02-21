dc090b61029d1b6cddd7124b5c858e0a99c16262
Victor Lin <bornstub@gmail.com>
2014-02-21 12:28:21 +0800

    Try to fix dependencies issue

--- config/software/python.rb
+++ config/software/python.rb
@@ -23,7 +23,6 @@ dependency "ncurses"
 dependency "zlib"
 dependency "openssl"
 dependency "bzip2"
-dependency "sqlite3"
 
 
 source :url => "http://python.org/ftp/python/#{version}/Python-#{version}.tgz",
@@ -40,7 +39,16 @@ build do
   command ["./configure",
            "--prefix=#{install_dir}/embedded",
            "--enable-unicode=ucs4",
-           "--enable-shared"].join(" "), :env => env
+           "--enable-shared",
+           # NOTICE: we will encounter dependencies check failure if the building
+           # environment happens to have these libraries installed. For solving
+           # this issue, I disable these module here. We can add these back
+           # once support for those library is ready. Reference to issue:
+           # https://github.com/opscode/omnibus-software/pull/70
+           "--without-sqlite3",
+           "--without-dbm",
+           "--without-gdbm",
+           ].join(" "), :env => env
   command "make", :env => env
   command "make install", :env => env
 
