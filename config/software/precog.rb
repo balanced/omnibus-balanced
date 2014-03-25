6d9414cad8f36558118460959c1aa2a9a5b2c92d
bninja <andrew@poundpay.com>
2014-03-25 14:00:59 -0700

    rm pip build dir on retries

--- config/software/precog.rb
+++ config/software/precog.rb
@@ -61,6 +61,5 @@ build do
   end
 
   temporary_build_dir = '/tmp/precog-build'
-  command "rm -rf #{temporary_build_dir}"
-  command "#{install_dir}/embedded/bin/pip install -v --no-use-wheel -b #{temporary_build_dir} --upgrade --install-option=--prefix=#{install_dir}/embedded .", env: env
+  command "rm -rf #{temporary_build_dir} && #{install_dir}/embedded/bin/pip install -v --no-use-wheel -b #{temporary_build_dir} --upgrade --install-option=--prefix=#{install_dir}/embedded .", env: env
 end
